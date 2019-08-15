db2fc276350f645d2d11883fc92bbc1542814178
Trang Le <tranglet@vn.fujitsu.com>
2019-08-15 01:42:02 -0700

    add support page

--- app/draw/urls.py
+++ app/draw/urls.py
@@ -10,7 +10,15 @@ urlpatterns = [
     path('connect/', views.connect),
     path('gallery/', views.gallery),
     path('support/', views.support),
-    #path('channel/index', views.index),
+    path('channel/index', views.index),
+    path('channel/channel', views.channel),
+    path('channel/connect', views.connect),
+<<<<<<< Updated upstream
+    path('channel/support', views.support),
+=======
+>>>>>>> Stashed changes
+    path('support/index', views.index),
+    path('support/channel', views.channel),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
 
